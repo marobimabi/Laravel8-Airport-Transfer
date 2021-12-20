2e391d6f4df84b6c6778f2b7b17491347c39e521
mahamat ali youssouf <56170343+marobimabi@users.noreply.github.com>
2021-12-21 01:28:05 +0300

    Setting
    admin setting acheved

--- resources/views/admin/setting_edit.blade.php
+++ resources/views/admin/setting_edit.blade.php
@@ -16,6 +16,10 @@
 
         <div class="row">
             <div class="col-md-12 col-sm-12  ">
+
+                <!-- start form for validation -->
+                <form  action="{{route('admin_setting_update')}}" method="post" >
+                    @csrf
                 <div class="x_panel">
                     <div class="x_title">
                         <h2><i class="fa fa-bars"></i> Tabs <small>Float left</small></h2>
@@ -37,100 +41,103 @@
 
                         <ul class="nav nav-tabs bar_tabs" id="myTab" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link active" id="general" data-toggle="tab" href="#general" role="tab" aria-controls="general" aria-selected="true">General</a>
+                                <a class="nav-link active" id="general-tab" data-toggle="tab" href="#general" role="tab" aria-controls="general" aria-selected="true">General</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" id="social" data-toggle="tab" href="#social" role="tab" aria-controls="social" aria-selected="false">Sept Email</a>
+                                <a class="nav-link" id="smtp-tab" data-toggle="tab" href="#smtp" role="tab" aria-controls="smtp" aria-selected="false">Sept Email</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Social media</a>
+                                <a class="nav-link" id="social-tab" data-toggle="tab" href="#social" role="tab" aria-controls="social" aria-selected="false">Social media</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">About As</a>
-
+                                <a class="nav-link" id="about-tab" data-toggle="tab" href="#about" role="tab" aria-controls="about" aria-selected="false">About As</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false"> Contact Page</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">References</a>
-
+                                <a class="nav-link" id="reference-tab" data-toggle="tab" href="#reference" role="tab" aria-controls="reference" aria-selected="false">References</a>
                             </li>
                         </ul>
                         <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                General
+                            <div class="tab-pane fade show active" id="general" role="tabpanel" aria-labelledby="general-tab">
+
+                                <input type="hidden" id="id" class="form-control" name="id" value="{{$data->id}}"  />
+
+                                <br />
+                                <label for="fullname">Title * :</label>
+                                <input type="text" id="fullname" class="form-control" name="title" value="{{$data->title}}"  />
+                                <br />
+                                <label for="fullname">Keywords * :</label>
+                                <input type="text" id="fullname" class="form-control" name="keywords" value="{{$data->keywords}}"  />
+                                <br />
+                                <label for="fullname">Description * :</label>
+                                <input type="text" id="fullname" class="form-control" name="description" value="{{$data->description}}"  />
+                                <br />
+                                <label for="fullname"> Company * :</label>
+                                <input type="text" id="fullname" value="10" class="form-control" name="company"  />
+                                <br />
+                                <label for="fullname">Address * :</label>
+                                <input type="text" id="fullname" class="form-control" name="address" value="{{$data->address}}"  />
+                                <br />
+                                <label for="fullname">phone * :</label>
+                                <input type="number" id="fullname" class="form-control" name="address"  value="{{$data->phone}}"  />
+                                <br />
+                                <label for="tax">Fax * :</label>
+                                <input value="{{$data->fax}}" id="fullname" class="form-control" name="fax" required />
+                                <br />
+                                <label for="tax">Email * :</label>
+                                <input type="detail" id="fullname" class="form-control" name="email" value="{{$data->email}}"  />
+                                <br />
                             </div>
-                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo
-                                booth letterpress, commodo enim craft beer mlkshk aliquip
+                            <div class="tab-pane fade" id="smtp" role="tabpanel" aria-labelledby="smtp-tab">
+                                <label for="fullname">Smtpserver * :</label>
+                                <input type="text" id="fullname" class="form-control" name="smtpserver" value="{{$data->smtpserver}}"  />
+                                <br />
+                                <label for="fullname">Smtppasword * :</label>
+                                <input type="text" id="fullname" class="form-control" name="smtppasword" value="{{$data->smtppasword}}"  />
+                                <br />
+                                <label for="fullname">Smtpport * :</label> 
+                                <input type="number" id="fullname" class="form-control" name="smtpport" value="{{$data->smtpport}}"  />
+                                <br />
+                            </div>
+                            <div class="tab-pane fade" id="social" role="tabpanel" aria-labelledby="social-tab">
+                                <label for="fullname">Facebook * :</label>
+                                <input type="text" id="fullname" class="form-control" name="facebook" value="{{$data->facebook}}"  />
+                                <br />
+                                <label for="fullname">Instagram * :</label>
+                                <input type="text" id="fullname" class="form-control" name="ınstagram" value="{{$data->ınstagram}}"  />
+                                <br />
+                                <label for="fullname">Twitter * :</label>
+                                <input type="text" id="fullname" class="form-control" name="twitter" value="{{$data->twitter}}"  />
+                            </div>
+                            <div class="tab-pane fade" id="about" role="tabpanel" aria-labelledby="about-tab">
+
+                                <label for="fullname">About Us * :</label>
+                                <textarea id="aboutus" class="form-control" name="aboutus">{$data->aboutus}}</textarea>
+                                <br />
                             </div>
                             <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-                                xxFood truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo
-                                booth letterpress, commodo enim craft beer mlkshk
+                                <label for="fullname">Contact * :</label>
+                                <textarea id="contac" class="form-control" name="contac">{$data->contac}}</textarea>
+                                <br />
                             </div>
+                            <div class="tab-pane fade" id="reference" role="tabpanel" aria-labelledby="reference-tab">
+                                <label for="fullname">References * :</label>
+                                <textarea id="references" class="form-control" name="references">{$data->references}}</textarea>
+                            </div>
+
                         </div>
                     </div>
                     <div class="x_content">
 
 
-                                <!-- start form for validation -->
-                                <form  action="{{route('admin_setting_update')}}" method="post" >
-                                  @csrf
 
-                                    <input type="hidden" id="id" class="form-control" name="id" value="{{$data->id}}"  />
 
-                                    <br />
-                                    <label for="fullname">Title * :</label>
-                                    <input type="text" id="fullname" class="form-control" name="title" value="{{$data->title}}"  />
-                                    <br />
-                                    <label for="fullname">Keywords * :</label>
-                                    <input type="text" id="fullname" class="form-control" name="keywords" value="{{$data->keywords}}"  />
-                                    <br />
-                                    <label for="fullname">Description * :</label>
-                                    <input type="text" id="fullname" class="form-control" name="description" value="{{$data->description}}"  />
-                                    <br />
-                                    <label for="fullname"> Company * :</label>
-                                    <input type="text" id="fullname" value="10" class="form-control" name="company"  />
-                                    <br />
-                                    <label for="fullname">Address * :</label>
-                                    <input type="text" id="fullname" class="form-control" name="address" value="{{$data->address}}"  />
-                                    <br />
-                                    <label for="fullname">phone * :</label>
-                                    <input type="number" id="fullname" class="form-control" name="address"  value="{{$data->phone}}"  />
-                                    <br />
-                                    <label for="tax">Fax * :</label>
-                                    <input value="{{$data->fax}}" id="fullname" class="form-control" name="fax" required />
-                                    <br />
-                                    <label for="tax">Email * :</label>
-                                    <input type="detail" id="fullname" class="form-control" name="email" value="{{$data->email}}"  />
-                                    <br />
-                                    <label for="fullname">Smtpserver * :</label>
-                                    <input type="text" id="fullname" class="form-control" name="smtpserver" value="{{$data->smtpserver}}"  />
-                                    <br />
-                                    <label for="fullname">Smtppasword * :</label>
-                                    <input type="text" id="fullname" class="form-control" name="smtppasword" value="{{$data->smtppasword}}"  />
-                                    <br />
-                                    <label for="fullname">Smtpport * :</label>
-                                    <input type="number" id="fullname" class="form-control" name="smtpport" value="{{$data->smtpport}}"  />
-                                    <br />
-                                    <label for="fullname">Facebook * :</label>
-                                    <input type="text" id="fullname" class="form-control" name="facebook" value="{{$data->facebook}}"  />
-                                    <br />
-                                    <label for="fullname">Instagram * :</label>
-                                    <input type="text" id="fullname" class="form-control" name="ınstagram" value="{{$data->ınstagram}}"  />
-                                    <br />
-                                    <label for="fullname">Twitter * :</label>
-                                    <input type="text" id="fullname" class="form-control" name="twitter" value="{{$data->twitter}}"  />
-                                    <br />
-                                    <label for="fullname">About Us * :</label>
-                                    <textarea id="aboutus" class="form-control" name="aboutus">{$data->aboutus}}</textarea>
-                                    <br />
-                                    <label for="fullname">Contact * :</label>
-                                    <textarea id="contac" class="form-control" name="contac">{$data->contac}}</textarea>
-                                    <br />
-                                    <label for="fullname">References * :</label>
-                                    <textarea id="references" class="form-control" name="references">{$data->references}}</textarea>
+
+
+
+
 
                                     <br/>
                                     <script>
