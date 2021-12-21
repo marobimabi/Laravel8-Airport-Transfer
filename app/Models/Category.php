2b04d68c6b2d890460d116fe4842482bb564a040
mahamat ali youssouf <56170343+marobimabi@users.noreply.github.com>
2021-12-21 03:57:57 +0300

    Category Product Relation
     Category Product Relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    public function transfer(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Transfer::class);
+
+    }
 }
