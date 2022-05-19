6d61b318b9bf89bab6584569bdaea2b3ba714dfa
mahamat ali youssouf <56170343+marobimabi@users.noreply.github.com>
2022-05-19 20:51:30 +0300

    Update _content.blade.php

--- resources/views/admin/_content.blade.php
+++ resources/views/admin/_content.blade.php
@@ -1,12 +1,3 @@
-<footer class="footer">
-    <div class="pull-right">
-        Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-    </div>
-    <div class="clearfix"></div>
-</footer>
-<!-- /footer content -->
-</div>
-</div>
 
 <!-- jQuery -->
 <script src="{{ asset('assets')}}/vendors/jquery/dist/jquery.min.js"></script>
